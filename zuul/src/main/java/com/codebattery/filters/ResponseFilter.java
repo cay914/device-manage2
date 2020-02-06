a64da5aaabb729bf313d1ac2aba46d6aaaf92ba8
cay914 <cay914@gmail.com>
2020-02-06 13:34:39 +0800

    修改zuul

---
+++ zuul/src/main/java/com/codebattery/filters/ResponseFilter.java
@@ -0,0 +1,76 @@
+package com.codebattery.filters;
+
+
+import com.codebattery.model.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonObjectSerializer;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+@Component
+public class ResponseFilter extends ZuulFilter{
+	
+    private static final int  FILTER_ORDER=1;
+    private static final boolean  SHOULD_FILTER=true;
+
+//    @Autowired
+//    FilterUtils filterUtils;
+
+    @Override
+    public String filterType() {
+        return FilterUtils.POST_FILTER_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FILTER_ORDER;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+    	RequestContext ctx = RequestContext.getCurrentContext();
+    	HttpServletRequest request = ctx.getRequest();
+    	
+    	StringBuffer requestURL = request.getRequestURL();
+    	if(requestURL!=null && 
+    			( requestURL.toString().contains("swagger-ui.html") 
+    					|| requestURL.toString().contains("api-docs"))) {
+    		return false;	//不拦截doc请求
+    	}
+    	
+        return SHOULD_FILTER;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.getResponse().addHeader(FilterUtils.CORRELATION_ID, FilterUtils.getCorrelationId());
+        
+        InputStream stream = ctx.getResponseDataStream();
+        String bodyStr;
+		try {
+			ObjectMapper mapper = new ObjectMapper();  
+			bodyStr = StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
+			HashMap jsonMap = mapper.readValue(bodyStr, HashMap.class); 
+			Response response = new Response("200", "", jsonMap);
+			
+			ctx.setResponseBody(mapper.writeValueAsString(response));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+        return null;
+    }
+}
