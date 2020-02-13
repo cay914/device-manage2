1b3b8c25f55ac9f88987911796c90d172ad1f907
tancaisheng <caisheng.tan@genlot.com>
2020-02-14 02:38:38 +0800

    修改zuul网关返回的response

--- zuul/src/main/java/com/codebattery/filters/ResponseFilter.java
+++ zuul/src/main/java/com/codebattery/filters/ResponseFilter.java
@@ -2,6 +2,8 @@ package com.codebattery.filters;
 
 
 import com.codebattery.model.Response;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
@@ -14,6 +16,9 @@ import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.util.JSONTokener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.stereotype.Component;
@@ -60,16 +65,26 @@ public class ResponseFilter extends ZuulFilter{
         
         InputStream stream = ctx.getResponseDataStream();
         String bodyStr;
+        Response response = null;
 		try {
-			ObjectMapper mapper = new ObjectMapper();
 			bodyStr = StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
-            /*
-             *   使用#占位符，body的json字符串直接替换"#"
-             *
-             */
-			Response response = new Response("200", "", "#");
-            String responseStr = mapper.writeValueAsString(response);
-			ctx.setResponseBody(responseStr.replaceAll("\"#\"", bodyStr));
+			if(bodyStr==null || bodyStr.isEmpty()){
+                response = new Response("200", "", "");
+            } else {
+                Object json = new JSONTokener(bodyStr).nextValue();
+                if(json instanceof JSONObject){
+                    JSONObject jsonObject = (JSONObject)json;
+                    response = new Response("200", "", jsonObject);
+                }else if (json instanceof JSONArray){
+                    JSONArray jsonArray = (JSONArray)json;
+                    response = new Response("200", "", jsonArray);
+                } else {
+                    response = new Response("200", "", bodyStr);
+                }
+            }
+
+            JSONObject responseJson = JSONObject.fromObject(response);
+			ctx.setResponseBody(responseJson.toString());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
