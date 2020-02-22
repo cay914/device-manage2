0b11ea58f6baf0f477ff2e4c93a778ee8671d7b7
cay914 <cay914@gmail.com>
2020-02-23 01:12:41 +0800

    解决数据截断问题

--- gateway/src/main/java/com/codebattery/filter/ApiResponseFilter.java
+++ gateway/src/main/java/com/codebattery/filter/ApiResponseFilter.java
@@ -2,6 +2,7 @@ package com.codebattery.filter;
 
 import com.codebattery.domain.model.Response;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.primitives.Bytes;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.sf.json.util.JSONTokener;
@@ -10,16 +11,17 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.NettyDataBuffer;
 import org.springframework.core.io.buffer.NettyDataBufferFactory;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import reactor.netty.Connection;
 
 import java.io.UnsupportedEncodingException;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.CLIENT_RESPONSE_CONN_ATTR;
 
@@ -41,59 +43,82 @@ public class ApiResponseFilter implements GatewayFilterFactory<ApiResponseFilter
         String bodyStr;
         Response response = null;
 
-            bodyStr = str;
-            if(bodyStr==null || bodyStr.isEmpty()){
-                response = new Response("200", "", "");
+        bodyStr = str;
+        if(bodyStr==null || bodyStr.isEmpty()){
+            response = new Response("200", "", "");
+        } else {
+            Object json = new JSONTokener(bodyStr).nextValue();
+            if(json instanceof JSONObject){
+                JSONObject jsonObject = (JSONObject)json;
+                response = new Response("200", "", jsonObject);
+            }else if (json instanceof JSONArray){
+                JSONArray jsonArray = (JSONArray)json;
+                response = new Response("200", "", jsonArray);
             } else {
-                Object json = new JSONTokener(bodyStr).nextValue();
-                if(json instanceof JSONObject){
-                    JSONObject jsonObject = (JSONObject)json;
-                    response = new Response("200", "", jsonObject);
-                }else if (json instanceof JSONArray){
-                    JSONArray jsonArray = (JSONArray)json;
-                    response = new Response("200", "", jsonArray);
-                } else {
-                    response = new Response("200", "", bodyStr);
-                }
+                response = new Response("200", "", bodyStr);
             }
+        }
 
-            JSONObject responseJson = JSONObject.fromObject(response);
-            jsonResponse = responseJson.toString().getBytes();
+        JSONObject responseJson = JSONObject.fromObject(response);
+        jsonResponse = responseJson.toString().getBytes();
 
         return jsonResponse;
+    }
+
+    private void cleanup(ServerWebExchange exchange) {
+        Connection connection = exchange.getAttribute(CLIENT_RESPONSE_CONN_ATTR);
+        if (connection != null) {
+            connection.dispose();
+        }
     }
 
     @Override
     public GatewayFilter apply(Config config) {
 
+        return ((exchange, chain) -> {
+            return chain.filter(exchange)
+                    .doOnError(throwable -> cleanup(exchange))
+                    .then(Mono.defer(() -> {
+                Connection connection = exchange.getAttribute(CLIENT_RESPONSE_CONN_ATTR);
+                if (connection == null) {
+                    return Mono.empty();
+                }
+                ServerHttpResponse response = exchange.getResponse();
+                NettyDataBufferFactory factory = (NettyDataBufferFactory) response.bufferFactory();
+                Mono<DataBuffer> body =
+                        connection
+                                .inbound()
+                                .receive()
+                                .retain()
+                                .map(factory::wrap)
+                                .collectList()
+                                .map(byteBufs -> {
 
-        return (exchange, chain) -> {
-                return chain.filter(exchange).then(Mono.defer(() -> {
-                    ServerHttpResponse response = exchange.getResponse();
-                    NettyDataBufferFactory factory = (NettyDataBufferFactory) response.bufferFactory();
-                    Flux<DataBuffer> body = Flux.just(factory.allocateBuffer());
-                    body.map(dataBuffer -> {
-                                byte[] content = new byte[dataBuffer.readableByteCount()];
-                                dataBuffer.read(content);
-                                DataBufferUtils.release(dataBuffer);
-                                String str = null;
-                                try {
-                                    str = new String(content, "UTF-8");
-                                } catch (UnsupportedEncodingException e) {
-                                    e.printStackTrace();
-                                }
-                                return str;
-                            }).collect(Collectors.joining(""))
-                            .map(str -> {
-                                DataBuffer dataBufferResp = factory.allocateBuffer();
-                                byte[] bytes = converToJson(str);
-                                dataBufferResp.write(bytes);
-                                return dataBufferResp;
-                            });
+                                    List<Byte> list = new ArrayList<>();
 
-                    return response.writeWith(body);
-                }));
-        };
+                                    byte [] bytes = new byte[0];
+
+                                    for( NettyDataBuffer buf : byteBufs) {
+                                        byte[] content = new byte[buf.readableByteCount()];
+                                        buf.read(content);
+                                        DataBufferUtils.release(buf);
+                                        bytes= Bytes.concat(bytes, content);
+                                    }
+
+                                    DataBuffer dataBufferResp = factory.allocateBuffer();
+                                    try {
+                                        byte[] bodys  = converToJson(new String(bytes, "utf-8"));
+                                        dataBufferResp.write(bodys);
+                                    } catch (UnsupportedEncodingException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return dataBufferResp;
+                                })
+                                .doOnCancel(() -> cleanup(exchange));
+
+                return response.writeWith(body);
+            }));
+        });
     }
 
     @Override
